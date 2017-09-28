f6a9c5d86b5a3abe663f11a320052711c83d47f5
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-27 14:14:42 -1000

    first test written

--- test/test.js
+++ test/test.js
@@ -2,3 +2,12 @@ const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
 const StringBuilder = require('../stringBuilder');
+
+describe ('StringBuilder', function () {
+  it('should be a function', function () {
+    expect(StringBuilder).to.be.a('function');
+  });
+
+
+
+});
